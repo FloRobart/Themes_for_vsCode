5b6a60ff8eb1d209b2d4a09d397910c6ea880ed2
Floris <florisrobart.pro@gmail.com>
2023-07-01 03:05:59 +0200

    propreter du code + correction de bug

--- installateur.sh
+++ installateur.sh
@@ -61,18 +61,18 @@ demandeInstallationThemePerso()
 #================================================#
 # Vérification de l'installation de github thème #
 #================================================#
-verifGithubThemeInstallation()
+verifExtentionGithubInstallation()
 {
-    verifGithubThemeInstallation=$(code --list-extensions | grep 'GitHub.github-vscode-theme' > /dev/null 2>&1 && echo 0 || echo 1)
-    if [ $verifGithubThemeInstallation -ne 0 ]
+    verifExtentionGithubInstallation=$(code --list-extensions | grep 'GitHub.github-vscode-theme' > /dev/null 2>&1 && echo 0 || echo 1)
+    if [ $verifExtentionGithubInstallation -ne 0 ]
     then
         echo 'L'\''extention Github thème n'\''est pas installé et est obligatoire pour installé le thème personnalisé. Voulez-vous installer l'\''extention ? (y/n)'
         read reponse
         if [ $reponse = "y" ] || [ $reponse = "Y" ] || [ $reponse = "yes" ] || [ $reponse = "Yes" ] || [ $reponse = "YES" ]
         then
             echo 'Installation du thème Github'
-            # Installation du thème Github
-            installationGithubTheme && return 0 || return 1
+            # Installation de l'extention Github thème
+            installationExtentionGithubTheme && return 0 || return 1
         else
             echo 'L'\''extention Github thème ne sera pas installé'
             return 1
@@ -87,8 +87,9 @@ verifGithubThemeInstallation()
 #=============================#
 # Intallation de github thème #
 #=============================#
-installationGithubTheme()
+installationExtentionGithubTheme()
 {
+    #for test in github.copilot-*; do echo $test; done
     code --install-extension GitHub.github-vscode-theme && ( echo 'Le thème Github à été installé avec succès' & return 0 ) || ( echo 'Une erreur s'est produite lors de l'installation du thème Github' & return 1 )
 }
 
@@ -118,4 +119,4 @@ ajoutThemeInFichierConfiguration()
 # Main #
 #======#
 # Vérification de l'installation de vscode
-( verifVsCodeInstallation && demandeInstallationThemePerso && verifGithubThemeInstallation && copieFichierTheme && ajoutThemeInFichierConfiguration ) && echo 'Installation réussi' || echo 'Une erreur s'\''est produite lors de l'\''installation'
+( verifVsCodeInstallation && demandeInstallationThemePerso && verifExtentionGithubInstallation && copieFichierTheme && ajoutThemeInFichierConfiguration ) && echo 'Installation réussi' || echo 'Une erreur s'\''est produite lors de l'\''installation'
