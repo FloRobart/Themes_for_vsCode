b9c9fe96c7f7d597f60b8fcc47dc9e07e1e24d97
Floris <florisrobart.pro@gmail.com>
2023-07-05 01:17:43 +0200

    copie du thème dans le bon dossier

--- installateur.sh
+++ installateur.sh
@@ -8,14 +8,14 @@
 #==========================================#
 # Vérification de l'installation de vscode #
 #==========================================#
-verifVsCodeInstallation()
+function verifVsCodeInstallation()
 {
     verifVsCodeInstallation=$(code --version > /dev/null 2>&1 && echo 0 || echo 1)
     if [ $verifVsCodeInstallation -ne 0 ]
     then
         echo 'Visual Studio Code n'\''est pas installé, Voulez-vous l'\''installer ? (y/n)'
         read reponse
-        if [ $reponse = "y" ] || [ $reponse = "Y" ] || [ $reponse = "yes" ] || [ $reponse = "Yes" ] || [ $reponse = "YES" ]
+        if [[ $answer =~ ^y(es)?$ ]]
         then
             echo 'Installation de VsCode...'
             # Installation de VsCode
@@ -33,9 +33,9 @@ verifVsCodeInstallation()
 #========================#
 # Installation de vscode #
 #========================#
-installationVsCode()
+function installationVsCode()
 {
-    sudo apt update && ( sudo snap install code --classic && ( echo 'VsCode est installé' & return 0 ) || ( echo 'Une erreur s'est produite lors de l'installation de VsCode' & return 1 ) )
+    sudo apt update && { sudo snap install code --classic && { echo 'VsCode est installé' ; return 0 ; } || { echo 'Une erreur s'est produite lors de l'installation de VsCode' ; return 1 ; } }
 }
 
 
@@ -43,11 +43,11 @@ installationVsCode()
 #=======================================#
 # Demande d'installation du thème perso #
 #=======================================#
-demandeInstallationThemePerso()
+function demandeInstallationThemePerso()
 {
     echo 'Voulez-vous installer le thème personnalisé ? (y/n)'
     read reponse
-    if [ $reponse = "y" ] || [ $reponse = "Y" ] || [ $reponse = "yes" ] || [ $reponse = "Yes" ] || [ $reponse = "YES" ]
+    if [[ $answer =~ ^y(es)?$ ]]
     then
         echo 'Installation du thème personnalisé...'
         return 0
@@ -61,14 +61,14 @@ demandeInstallationThemePerso()
 #================================================#
 # Vérification de l'installation de github thème #
 #================================================#
-verifExtentionGithubInstallation()
+function verifExtentionGithubInstallation()
 {
     verifExtentionGithubInstallation=$(code --list-extensions | grep 'GitHub.github-vscode-theme' > /dev/null 2>&1 && echo 0 || echo 1)
     if [ $verifExtentionGithubInstallation -ne 0 ]
     then
         echo 'L'\''extention Github thème n'\''est pas installé et est obligatoire pour installé le thème personnalisé. Voulez-vous installer l'\''extention ? (y/n)'
         read reponse
-        if [ $reponse = "y" ] || [ $reponse = "Y" ] || [ $reponse = "yes" ] || [ $reponse = "Yes" ] || [ $reponse = "YES" ]
+        if [[ $answer =~ ^y(es)?$ ]]
         then
             echo 'Installation du thème Github'
             # Installation de l'extention Github thème
@@ -87,178 +87,93 @@ verifExtentionGithubInstallation()
 #=============================#
 # Intallation de github thème #
 #=============================#
-installationExtentionGithubTheme()
+function installationExtentionGithubTheme()
 {
-    code --install-extension GitHub.github-vscode-theme && ( echo 'Le thème Github à été installé avec succès' & return 0 ) || ( echo 'Une erreur s'est produite lors de l'installation du thème Github' & return 1 )
+    code --install-extension GitHub.github-vscode-theme && { echo 'Le thème Github à été installé avec succès' ; return 0 ; } || { echo 'Une erreur s'est produite lors de l'installation du thème Github' ; return 1 ; }
 }
 
 
 #=====================================#
 # Copie du fichier contenant le thème #
 #=====================================#
-copieFichierTheme()
+function copieFichierTheme()
 {
-    # récupération de la dernière version de l'extention
-    # ~/.vscode/extensions/
-    ensFile=($(ls -d ./Tests/github.github-vscode-theme-*))
-    IFS=$'\n' ensFile=($(sort <<<"${ensFile[*]}")); unset IFS
+    #----------------------------------------------------#
+    # récupération de la dernière version de l'extention #
+    #----------------------------------------------------#
+    ensFiles=($(ls -d ./Tests/github.github-vscode-theme-*))
+    IFS=$'\n' ensFiles=($(sort <<<"${ensFiles[*]}")); unset IFS
 
     i=0
-    for value in "${ensFile[@]}"
+    for value in "${ensFiles[@]}"
     do
         # Séparation des versions
-        version[$i]=$(echo $value | sed 's/.*-//' | sed 's/\.//' | sed 's/\.//')
+        versions[$i]=$(echo $value | sed 's/.*-//')
+        versions[$i]=$(retirePoint ${versions[$i]})
         ((i++))
     done
 
-    printf '%s, ' "${version[@]}"
+    printf '%s, ' "${versions[@]}"
     echo -e
 
     # mise de toute les versions sur le même nombre de chiffre
-    IFS=$'\n' version=($(sort -n <<<"${version[*]}")); unset IFS
-
-    nbChiffre=${version[ ${#version[@]}-1 ]}
+    IFS=$'\n' versionsTrie=($(sort -n <<<"${versions[*]}")); unset IFS
+    nbChiffre=${versionsTrie[ ${#versions[@]}-1 ]}
     i=0
-    for value in "${version[@]}"
+    for value in "${versionsTrie[@]}"
     do
-        if [ $nbChiffre -ne $value ]
-        then
-            version[$i]=${version[$i]}0
-        fi
+        [[ $nbChiffre -ne $value ]] && versionsTrie[$i]="${versionsTrie[$i]}"'0'
         ((i++))
     done
 
-    IFS=$'\n' version=($(sort -n <<<"${version[*]}")); unset IFS
+    IFS=$'\n' versionsTrie=($(sort -n <<<"${versionsTrie[*]}")); unset IFS
 
-    printf '%s, ' "${version[@]}"
+    lastVersion=${versionsTrie[ ${#versions[@]}-1 ]}
+
+    printf '%s, ' "${versionsTrie[@]}"
     echo -e
+    echo 'dernière version --> '"$lastVersion"
 
-    # Récupération du fichier avec le bon numéro de version
+    # récupération du nom du dossier le plus récent
+    getFolderNameLastversion
 
-: << 'END'
-    #---------------------------------------------------#
-    # récupération des versions des extentions trouvées #
-    #---------------------------------------------------#
-    i=0
-    for value in "${ensFile[@]}"
-    do
-        # Séparation des versions (majeur, mineur, patch)
-        version=$(echo $value | sed 's/.*-//')
-        majeur[$i]=$(echo $version | sed 's/\.[0-9]*\.[0-9]*$//') # version majeur
-        mineur[$i]=$(echo $version | sed 's/^[0-9]*\.//' | sed 's/\.[0-9]*$//') # version mineur
-        patch[$i]=$(echo $version | sed 's/^[0-9]*\.[0-9]*\.//') # patch
+    [[ -z $lastFolder ]] && { lastVersion=$( sed 's/0$//' <<<"$lastVersion" ) ; getFolderNameLastversion ; }
+    
+    echo 'dernière version --> '"$lastVersion"
 
-        #echo $version ' --> ' ${majeur[$i]}, ${mineur[$i]}, ${patch[$i]}
-        ((i++))
-    done
-
-
-    #---------------------------------------------#
-    # Suppression des versions les moins récentes #
-    #---------------------------------------------#
-    IFS=$'\n' majeurSorted=($(sort -n <<<"${majeur[*]}")); unset IFS
-
-    printf '%s, ' "${majeur[@]}"
-    echo -e
-    printf '%s, ' "${mineur[@]}"
-    echo -e
-    printf '%s, ' "${patch[@]}"
-    echo -e "\n"
-
-    i=0
-    lastMajeur=${majeurSorted[ ${#majeurSorted[@]}-1 ]} # dernière version majeur
-    for value in "${majeur[@]}"
-    do
-        if [ $value -ne $lastMajeur ]
-        then
-            unset majeur[$i]
-            unset mineur[$i]
-            unset patch[$i]
-        fi
-        ((i++))
-    done
-
-    echo 'Version majeur :'
-    printf '%d, ' "${majeur[@]}"
-    echo -e
-    printf '%d, ' "${mineur[@]}"
-    echo -e
-    printf '%d, ' "${patch[@]}"
-    echo -e "\n"
-
-    IFS=$'\n' mineurSorted=($(sort -n <<<"${mineur[*]}")); unset IFS
-
-    i=0
-    lastMineur=${mineurSorted[ ${#mineurSorted[@]}-1 ]} # dernière version mineur
-    for value in "${mineur[@]}"
-    do
-        if [ $value -ne $lastMineur ]
-        then
-            unset majeur[$i]
-            unset mineur[$i]
-            unset patch[$i]
-        fi
-        ((i++))
-    done
-
-    echo 'Version mineur :'
-    printf '%d, ' "${majeur[@]}"
-    echo -e
-    printf '%d, ' "${mineur[@]}"
-    echo -e
-    printf '%d, ' "${patch[@]}"
-    echo -e "\n"
-
-    IFS=$'\n' patchSorted=($(sort -n <<<"${patch[*]}")); unset IFS
-
-    ((i=0))
-    lastPatch=${patchSorted[ ${#patchSorted[@]}-1 ]} # dernière version patch
-    for value in "${patch[@]}"
-    do
-        if [ $value -ne $lastPatch ]
-        then
-            unset majeur[$i]
-            unset mineur[$i]
-            unset patch[$i]
-        fi
-        ((i++))
-    done
-
-    echo 'Version patch :'
-    printf '%d, ' "${majeur[@]}"
-    echo -e
-    printf '%d, ' "${mineur[@]}"
-    echo -e
-    printf '%d, ' "${patch[@]}"
-    echo -e "\n"
-
+    echo 'dossier de la dernière version --> '"$lastFolder"
 
     #-------------------------------------#
-    # récupération de la dernière version #
+    # Copie du fichier contenant le thème #
     #-------------------------------------#
-
-
-
-    #--------------------------------#
-    # récupération du nom du fichier #
-    #--------------------------------#
-
-
-
-    #------------------------------------------#
-    # copie du fichier dans le dossier .vscode #
-    #------------------------------------------#
-    #cp ./Themes/dark-perso.json ~/.vscode/extensions/$finalFile/themes/dark-perso.json && return 0 || return 1
-
-END
+    echo 'Copie du fichier contenant le thème'
+    cp "./Themes/dark-perso.json" "~/.vscode/extensions/$lastFolder/themes/dark-perso.json"
 }
 
+function getFolderNameLastversion()
+{
+    i=0
+    for value in "${ensFiles[@]}"
+    do
+        if [[ $(retirePoint ${versions[$i]}) -eq $lastVersion ]]
+        then
+            lastFolder=$value
+            break
+        fi
+        ((i++))
+    done
+}
+
+function retirePoint()
+{
+    echo $( echo $1 | sed 's/\.//' | sed 's/\.//' )
+}
 
 
 #============================================#
 # Ajout du thème au fichier de configuration #
 #============================================#
-ajoutThemeInFichierConfiguration()
+function ajoutThemeInFichierConfiguration()
 {
     echo 'Ajout du thème au fichier de configuration'
     return 0
@@ -272,6 +187,7 @@ ajoutThemeInFichierConfiguration()
 # Vérification de l'installation de vscode
 
 # test
+verifExtentionGithubInstallation
 copieFichierTheme
 
 # commande main final
