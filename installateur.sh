f2ba0e02a4cda208fe219010f2670129d5266171
Floris <florisrobart.pro@gmail.com>
2023-07-06 02:16:27 +0200

    correction de bug

--- installateur.sh
+++ installateur.sh
@@ -118,7 +118,7 @@ function copieFichierTheme()
     #-------------------------------------#
     # Copie du fichier contenant le thème #
     #-------------------------------------#
-    cp "./Themes/dark-perso.json" "~/.vscode/extensions/$lastFolder/themes/dark-perso.json"
+    sudo cp "./Themes/dark-perso.json" "~/.vscode/extensions/$lastFolder/themes/dark-perso.json"
 }
 
 function getFolderNameLastversion()
