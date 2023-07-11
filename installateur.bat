dca88faf49b6f21db43573cd2d76becb72d15e49
Floris <florisrobart.pro@gmail.com>
2023-07-11 19:50:38 +0200

    up

--- installateur.bat
+++ installateur.bat
@@ -45,9 +45,9 @@ goto :EOF
     call code --version >nul 2>&1 && ( set /a "verifVsCodeInstallation=0" ) || ( set /a "verifVsCodeInstallation=1" )
 
     if "!verifVsCodeInstallation!" NEQ "0" (
-        set /p "reponse=Visual Studio Code n'est pas installé, Voulez-vous l'installer ? ^(y/n^) ^: "
+        set /p "reponse=Visual Studio Code n'est pas installé, Voulez-vous l'installer ? (y/n) : "
 
-        echo %reponse%
+        echo !reponse!
         echo %reponse% | findstr /R "^y(es)?$" && ( call :installationVsCode & echo Installation de Visual Studio Code & goto :EOF) || ( echo Visual studio Code ne sera pas installe & set /a "erreur=1" & set /a "verifVsCodeInstallation=1" & goto :EOF )
     ) else (
         echo Visual studio Code est déjà installé
