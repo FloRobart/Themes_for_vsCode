080b98251d4c6ce60efc4b89a1e04525c25a389d
Floris <florisrobart.pro@gmail.com>
2023-07-13 16:29:36 +0200

    correction de bug

--- installateur.bat
+++ installateur.bat
@@ -69,7 +69,7 @@ goto :EOF
 :installationVsCode
     echo Une fois le telechargement termine, fermer la fenetre du navigateur pour continuer l'installation
     start /wait https://code.visualstudio.com/docs/?dv=win && (
-        for /f "USEBACKQ tokens=*" %%a in (`dir /B /O-D "C:\Users\%USERNAME%\Downloads\VSCodeUserSetup-x64-*.exe"`) do set "file=%%a"
+        for /f "USEBACKQ tokens=*" %%a in (`dir /B /OD "'C:\Users\%USERNAME%\Downloads\VSCodeUserSetup-x64-*.exe'"`) do set "file=%%a"
         start /wait /D "C:\Users\%USERNAME%\Downloads\" !file!
         echo Installation de Visual Studio Code terminee
     ) || (
