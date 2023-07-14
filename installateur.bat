a650d23a5aaa4d1ec699a6f59a19b2e7434b4c69
Floris <florisrobart.pro@gmail.com>
2023-07-14 18:40:40 +0200

    test

--- installateur.bat
+++ installateur.bat
@@ -55,9 +55,9 @@ goto :EOF
     echo Une fois le telechargement termine, fermer la fenetre du navigateur pour continuer l'installation
     start /wait https://code.visualstudio.com/docs/?dv=win && (
         for /f "USEBACKQ tokens=*" %%a in (`dir /B /O-D "%HomeDrive%%HomePath%\Downloads\VSCodeUserSetup-x64-*.exe"`) do set "file=%%a"
+        start /wait cmd /C code --version >nul 2>&1 && ( echo Visual Studio Code a ete installe & set /a "installationVsCode=0" ) || ( echo Visual Studio Code n'a pas ete installe & set /a "installationVsCode=1" )
         start /wait /D "%HomeDrive%%HomePath%\Downloads\" !file!
         
-        start /wait cmd /C code --version >nul 2>&1 && ( echo Visual Studio Code a ete installe & set /a "installationVsCode=0" ) || ( echo Visual Studio Code n'a pas ete installe & set /a "installationVsCode=1" )
     ) || (
         echo Une erreur s'est produite lors de l'installation de Visual Studio Code
         set /a "installationVsCode=1"
