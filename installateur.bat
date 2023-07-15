940c81169d20b9af2c3af094af789b5a4264de63
Floris <florisrobart.pro@gmail.com>
2023-07-15 14:06:20 +0200

    test

--- installateur.bat
+++ installateur.bat
@@ -58,9 +58,10 @@ goto :EOF
         for /f "USEBACKQ tokens=*" %%a in (`dir /B /O-D "%HomeDrive%%HomePath%\Downloads\VSCodeUserSetup-x64-*.exe"`) do set "file=%%a"
         start /wait /D "%HomeDrive%%HomePath%\Downloads\" !file!
 
-        start /wait cmd /C (( code --version >nul 2>&1 ) && ( echo Visual Studio Code a ete installe & set /a "test=0" ) || ( echo Visual Studio Code n'a pas ete installe & set /a "test=1" ))
+        start /wait cmd /C ( code --version >nul 2>&1 && ( echo Visual Studio Code a ete installe & set /a "test=0" & pause ) || ( echo Visual Studio Code n'a pas ete installe & set /a "test=1" & pause ))
 
         echo test '!test!'
+
     ) || (
         echo Une erreur s'est produite lors de l'installation de Visual Studio Code
         set /a "installationVsCode=1"
